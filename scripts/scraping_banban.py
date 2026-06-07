a0b75a9606fad4cfd5b2956a7a7158d0bc03a4c5
disk0403 <daipi2003@gmail.com>
2026-06-07 10:54:10 +0900

    Update scraping_banban.py

--- scripts/scraping_banban.py
+++ scripts/scraping_banban.py
@@ -3,7 +3,17 @@ import bs4
 import requests
 from dict_pref_city import dict_pref, dict_city
 
-rows = []
+branch_info_rows = []
+branch_condition_rows = []
+branch_code = 0
+chain_code = 1
+condition_code_by_alt = {
+    "専用・共有駐車場": 1,
+    "提携駐車場": 1,
+    "Wi-Fi": 2,
+    "持ち込みOK": 3,
+    "喫煙所": 4,
+}
 
 url = "https://karaoke-shin.jp/shop-list/#freeResearch"
 r = requests.get(url)
@@ -43,28 +53,31 @@ for link_pref in s.find_all("area", shape = "rect"):
     pref = s_pref.find("div", class_ = "breadlist").find_all("li")[2].string
 
     for info_branch in s_pref.find_all("li", class_ = "clearfix"):
+        branch_code += 1
         branch = info_branch.a.string
         url_branch = info_branch.a["href"]
         address = get_address(info_branch, pref)
         city = get_city(address, pref)
-        parking, wifi, bringin, smoking = 0, 0, 0, 0
+        condition_codes = []
 
         for img_tag in info_branch.find_all("img"):
             alt = img_tag.get("alt")
-            if alt == "専用・共有駐車場":
-                parking = 1
-            elif alt == "提携駐車場":
-                parking = 1
-            elif alt == "Wi-Fi":
-                wifi = 1
-            elif alt == "持ち込みOK":
-                bringin = 1
-            elif alt == "喫煙所":
-                smoking = 1
+            condition_code = condition_code_by_alt.get(alt)
 
-        rows.append(["カラオケBanBan", branch, url_branch, pref, city, address, parking, wifi, bringin, smoking])
+            if condition_code:
+                condition_codes.append(condition_code)
 
-with open("local/branches.csv", "w", newline="", encoding="utf-8-sig") as f:
+        for condition_code in sorted(set(condition_codes)):
+            branch_condition_rows.append([branch_code, condition_code])
+
+        branch_info_rows.append([branch_code, chain_code, branch, url_branch, pref, city, address])
+        
+with open("local/branch_info.csv", "w", newline="", encoding="utf-8-sig") as f:
     writer = csv.writer(f)
-    writer.writerow(["chain", "branch", "url", "pref", "city", "address", "parking", "wifi", "bringin", "smoking"])
-    writer.writerows(rows)
+    writer.writerow(["branch_code", "chain_code", "branch", "url", "pref", "city", "address"])
+    writer.writerows(branch_info_rows)
+
+with open("local/branch_condition.csv", "w", newline="", encoding="utf-8-sig") as f:
+    writer = csv.writer(f)
+    writer.writerow(["branch_code", "condition_code"])
+    writer.writerows(branch_condition_rows)
